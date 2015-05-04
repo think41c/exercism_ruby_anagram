96eb7c9478a97473fd54abfd9278087122e44b59
Tony DiNitto <think41c@gmail.com>
2015-05-04 18:59:31 -0400

    Pseudo code.

--- anagram.rb
+++ anagram.rb
@@ -1,8 +1,22 @@
 class Anagram
-  def initialize(x)
+  def initialize(anagram)
+    # Take in the word as a string.
+    # Find the length of the string
+    # Extract the letters into an array
   end
 
-  def match(y)
+  def match(matches)
+    # match = []
+    # Loop through each word in the matches array
+    #   Find the length of the word in the array
+    #   Loop as many times are there are letters in the word
+    #     Check and see if the first letter of anagram is same as match word
+    #       If yes, check the next one. Correct letters += 1
+    #         If correct letters == anagram lenght then add word in 'match'
+    #       Else 
+    #         Go to the next word
+    #       End
+
     a = []
     a
   end
