e184f3e8c5d225089a425392dcaad908080778d0
Tony DiNitto <think41c@gmail.com>
2015-05-05 22:46:04 -0400

    Prelim logic for dealing with case sensitive problems established.'

--- anagram.rb
+++ anagram.rb
@@ -13,7 +13,26 @@ class Anagram
       match << matches[counter] if word_to_examine == @anagram_lets
       counter += 1
     end
-    match.delete(@anagram)
-    p match
+
+    x = 0 
+    until x == match.length 
+      if match[x].downcase == @anagram.downcase
+        p match 
+        puts "they're the same!"
+        match.delete_at(x)
+        p "just deleted it"
+        p match
+      else
+        p match
+        puts "Not the same!"
+      end
+    x += 1 
+    end
+
+    match.delete(@anagram.downcase)
+    match
   end
 end
+
+detector = Anagram.new('allergy')
+anagrams = detector.match %w( allergy gallery ballerina regally clergy largely leading)
