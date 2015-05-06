48f1c7e96a7f2d04630b27b672cc6b9fedc920af
Tony DiNitto <think41c@gmail.com>
2015-05-05 22:06:41 -0400

    Refactor and cleaning code.

--- anagram.rb
+++ anagram.rb
@@ -12,23 +12,9 @@ class Anagram
     counter = 0
     until counter == matches.length
       word_to_examine = matches[counter].split("").sort
-      p word_to_examine
-      if word_to_examine == @anagram_lets
-        puts "They're the same!"
-        match << matches[counter]
-      else
-        puts "They're not the same!"
-      end
-
+      match << matches[counter] if word_to_examine == @anagram_lets
       counter += 1
     end
-    #   Loop as many times are there are letters in the word
-    #     Check and see if the first letter of anagram is same as match word
-    #       If yes, check the next one. Correct letters += 1
-    #         If correct letters == anagram lenght then add word in 'match'
-    #       Else 
-    #         Go to the next word
-    #       End
     match
   end
 
