f93f62b6cf3d8c445f76f909bb54a662be9e12d3
Tony DiNitto <think41c@gmail.com>
2015-05-06 14:50:13 -0400

    Cleaned up puts statements.

--- anagram.rb
+++ anagram.rb
@@ -15,33 +15,15 @@ class Anagram
     end
 
     x = 0 
-    puts "We're starting with the 'matchz' array - #{matchz}"
-    puts "matchz length -> #{matchz.length} , anagram downcase -> #{@anagram.downcase}, x ->#{x}"
-    
     until x >= matchz.length 
-     
-
         if matchz[x].downcase == @anagram.downcase
-          puts "\"#{matchz[x]}\" <- Matchz[x] and \"#{@anagram.downcase}\" - they're the same!"
-          p "just deleted - #{matchz[x]}"
           matchz.delete_at(x)
-          p matchz
-          p "*********"
           x = 0
-          puts "The matchz lenght #{matchz.length}"
         else
-          puts "Not the same!"
-          p matchz
           x += 1 
         end
-        puts "Still in until loop."
-      
       end
-
     matchz.delete(@anagram.downcase)
     matchz
   end
 end
-detector = Anagram.new('corn')
-anagrams = detector.match %w(corn dark Corn rank CORN cron park)
-p anagrams
